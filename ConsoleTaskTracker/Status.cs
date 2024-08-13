0770d44b9d14f7187d9cdbdab17a68a37ab9de82
Matěj Svoboda <svobodamatej97@gmail.com>
2024-08-13 16:51:50 +0200

    Add project files.

---
+++ ConsoleTaskTracker/Status.cs
@@ -0,0 +1,9 @@
+﻿namespace ConsoleTaskTracker
+{
+    public enum Status
+    {
+        Todo = 1,
+        InProgress = 2,
+        Done = 3
+    }
+}
---
+++ ConsoleTaskTracker/Task.cs
@@ -0,0 +1,20 @@
+﻿namespace ConsoleTaskTracker
+{
+    public class Task
+    {
+        public int TaskId { get; set; }
+        public string? Description { get; set; }
+        public Status Status { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+
+        public Task()
+        {
+            TaskId = Convert.ToInt32(Guid.NewGuid());
+            Description = null;
+            Status = Status.Todo;
+            CreatedAt = DateTime.Now;
+            UpdatedAt = DateTime.Now;
+        }
+    }
+}
